850f2facd6885646ed525c237b07eddc0efb3ef3
ariffatur17 <ariffatur30@gmail.com>
2021-04-18 05:24:30 +0700

    edited

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<?php
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://platform.antares.id:8443/~/antares-cse/antares-id/Smart_--Parking/ParkirSensor/la',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'GET',
-    CURLOPT_HTTPHEADER => array(
-        'X-M2M-Origin:  5b00cd16ad87ef4f:353456772f535c49',
-        'Content-Type:  application/json',
-        'Accept:  application/json'
-    ),
-    ));
-
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-    $response = json_decode($response, true);
-    // header("Access-Control-Allow-Origin: *");
-?>
 <!doctype html>
 <html class="no-js" lang="zxx">
 
